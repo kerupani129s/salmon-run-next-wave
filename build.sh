1644e262a1a8d45c33f77b1da761b4867f6ce3d1
ケルパニ＠猫 <32389989+kerupani129s@users.noreply.github.com>
2025-07-01 23:37:16 +0900

    Fix: 簡易版の DPS およびダメージの一覧において Markdown 記法が残っていたのを削除する修正

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$0")"
 cat docs/weapons/main/dps-list.md \
 	| sed --sandbox -E '1 s/^# (.*)$/\1 簡易版/; 2,/##/ { /##/ ! d }; /##/,$ { /^\t*-/ ! d }' \
 	| sed --sandbox -E '/バージョン [0-9.]+ 以降/ d' \
+	| sed --sandbox -E 's/\*\*//g' \
 	| sed --sandbox -E '/DPS.*= / { /= [0-9.]+$/ ! d; s/DPS.*= // }' \
 	| sed --sandbox -E '/^\t-/ { h; N; /\n\t\t-/ ! P; D }; /^\t\t-/ { G; s/^\t\t-(.*)\n(\t-.*)$/\2\1/ }' \
 	| sed --sandbox -E 's/2 周未満チャージ (1 周チャージ)/\1/' \
@@ -16,6 +17,7 @@ cat docs/weapons/main/dps-list.md \
 cat docs/weapons/main/damage-list.md \
 	| sed --sandbox -E '1 s/^# (.*)$/\1 簡易版/; 2,/##/ { /##/ ! d }; /##/,$ { /^\t*-/ ! d }' \
 	| sed --sandbox -E '/バージョン [0-9.]+ 以降/ d' \
+	| sed --sandbox -E 's/\*\*//g' \
 	| sed --sandbox -E \
 		-e '/射撃 ダメージ/ { /= [2-4] / s/ダメージ = //; s/ダメージ.*= // }' \
 		-e '/合計ダメージ/ { /= [2-7] / s/ダメージ =//; s/ダメージ.*=// }' \
